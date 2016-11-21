95c4b492cf983e6390812c95706eaa3b8dd2131b
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2016-11-21 21:08:39 +0300

    works with any resolution

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ void rotate_image();
 void rewrite_header();
 
 int main() {
-
+	
 	int size[2];
 	int offset;
 	int litter;
@@ -25,6 +25,9 @@ int main() {
 	fseek(input_image, 18, SEEK_SET);
 	fread(size, sizeof(int), 2, input_image);
 
+	litter = size[0] % 4;
+	printf("litter is %d px\n", litter);
+	
 	pixel_t* pixel_array = malloc(size[0] * size[1] * sizeof(pixel_t));
 	pixel_t* transformed_array = malloc(size[0] * size[1] * sizeof(pixel_t));
 
@@ -33,20 +36,30 @@ int main() {
 	fseek(input_image, 10, SEEK_SET);
 	fread(&offset, sizeof(int), 1, input_image);
 
-	read_array(pixel_array, input_image, image, offset);
+
+
+	read_array(pixel_array, input_image, image, offset, litter);
 
 	rotate_image(pixel_array, transformed_array, image);
 	rewrite_header(image, input_image);
 
 	fseek(input_image, offset, SEEK_SET);
-	fwrite(transformed_array, 3, image.width * image.height, input_image);
+	for(int i = 0; i < image.height; i++){
+		fwrite((transformed_array + i * image.width), 3, image.width, input_image);
+		fwrite(&size, litter, 1, input_image);
+	}
 	fclose(input_image);
 	return 0;
 }
 
-void read_array(pixel_t* array, FILE* input_image, image_t image, int offset){
+void read_array(pixel_t* array, FILE* input_image, image_t image, int offset, int litter){
+	char dev_null;
 	fseek(input_image, offset, SEEK_SET);
-	fread(array, 3, image.width * image.height, input_image);
+	for(int i = 0; i < image.height; i++){
+		printf("row is %d\n", i);
+		fread((array + (i * image.width)), 3, image.width, input_image);
+		fread(&dev_null, litter, 1, input_image);
+	}
 }
 
 void rotate_image(pixel_t* array, pixel_t* transformed_array, image_t image){
